3f8b5ea24fe7e6e81d5fae7f9ac77c762d19e334
Emre Özdemir <necatiiemre@gmail.com>
2023-10-10 00:05:52 +0300

    Update DependencyInjectionTest.java

--- bean/src/main/java/learnspring/bean/DependencyInjectionTest.java
+++ bean/src/main/java/learnspring/bean/DependencyInjectionTest.java
@@ -13,6 +13,7 @@ public class DependencyInjectionTest {
 	
 	public ConstructorInjectionBean constructorInjectionBean;
 	
+	@Autowired
 	public DependencyInjectionTest(ConstructorInjectionBean constructorInjectionBean) {
 		this.constructorInjectionBean = constructorInjectionBean;
 	}
